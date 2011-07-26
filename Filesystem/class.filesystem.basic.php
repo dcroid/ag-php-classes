27b7d419d26d8041f038e5095f664d11c80325b0
agbiggora <agbiggora@gmail.com>
2011-07-26 09:10:52 +0000

    Commit Filesystem

---
+++ Filesystem/class.filesystem.basic.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @revision      $Id:$   
+ * @created       Apr 22, 2011
+ * @category      category
+ * @package       package
+ * @version       version
+ * @copyright     Copyright Alexey Gordeyev IK © 2009-2011 - All rights reserved.
+ * @license       GPLv2
+ * @author        Alexey Gordeyev IK <aleksej@gordejev.lv>
+ * @link          http://www.agjoomla.com/classes/
+ */
+
+
+
+
+?>
