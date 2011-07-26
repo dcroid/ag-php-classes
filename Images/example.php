decef6a7008a11e0079bc6db6c8bed61b9a22b76
agbiggora <agbiggora@gmail.com>
2011-07-26 13:06:56 +0000

    Commit Helper example

---
+++ Images/example.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @revision      $Id:$   
+ * @created       Jul 22, 2011
+ * @package       Images
+ * @subpackage	  Helper
+ * @category      Utillites
+ * @version       1.0.0
+ * @copyright     Copyright Alexey Gordeyev IK © 2009-2011 - All rights reserved.
+ * @license       GPLv2
+ * @author        Alexey Gordeyev IK <aleksej@gordejev.lv>
+ * @link          http://www.agjoomla.com/classes/
+ */
+
+
+
+
+?>
