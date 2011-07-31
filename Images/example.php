9599ee2f094751e9d1b78063ef90e7eb5fc3a2cb
agbiggora <agbiggora@gmail.com>
2011-07-31 11:09:57 +0000

    Commit Helper and example

--- Images/example.php
+++ Images/example.php
@@ -19,12 +19,34 @@ ini_set('display_errors', '1');
 
 require_once 'class.images.helper.php';
 
-$image_source = './images/test.jpg';
+echo ' GIF: '.IMAGETYPE_GIF;
+echo ' JPEG: '.IMAGETYPE_JPEG;
+echo ' PNG: '.IMAGETYPE_PNG;
+echo ' BMP: '.IMAGETYPE_BMP;
+echo ' XBM: '.IMAGETYPE_XBM;
+echo ' WBMP: '.IMAGETYPE_WBMP;
+
+
+
+$image_source = './images/source.jpg';
 $new_image    = 'test.png';
 
 # get full information from image 
 $image_info = ImagesHelper::getimageinfo($image_source);
-
+var_dump($image_info);
+/*
+if(is_object($image_source)) {
+   echo "Is object";
+} else {
+   $image = ImagesHelper::createimagefromsource($image_source);
+   if(is_object($image)) {
+      echo "Cool is object";
+   } else {
+      echo gettype($image) . "\n";
+      echo get_resource_type($image) . "\n";
+   }
+}
+*/
 # get image width
 $width      = ImagesHelper::getimageinfo($image_source,'width');
 # or
@@ -40,7 +62,6 @@ $mime       = ImagesHelper::getimageinfo($image_source,'mime');
 # or
 $mime       = ImagesHelper::mime($image_source);
 
-
 # Convert hex color to rgb
 $hex_color = '#cccccc';
 $rgb_color = ImagesHelper::hextorgb($hex_color);
@@ -48,11 +69,20 @@ $rgb_color = ImagesHelper::hextorgb($hex_color);
 
 # Convert image format
 ImagesHelper::convert($image_source,'png');
+ImagesHelper::convert($image_source,'wbmp');
+ImagesHelper::convert($image_source,'xbm');
 
 # Convert image to grayscale
 if($grayscale_image = ImagesHelper::colortograyscale($image_source)) {
    # save grayscale image in to file in gif format
    ImagesHelper::saveimage($grayscale_image, $image_source,'bw_','gif');
+}
+
+# Convert image to grayscale
+if($grayscale_image = ImagesHelper::colortograyscale($image_source)) {
+   $grayscale_image  = ImagesHelper::imagetonegative($grayscale_image);
+   # save grayscale image in to file in gif format
+   ImagesHelper::saveimage($grayscale_image, $image_source,'bw2_','gif');
 }
 
 # Convert image to negative
@@ -112,10 +142,10 @@ if($image_with_reflection  = ImagesHelper::buildimagewithreflection($image_sourc
 # build faded image
 $alpha_start = 120;
 $alpha_end = 0;
-$bg_color = "#7E58BF"; //   #7E58BF
+$bg_color = "#000"; //   #7E58BF
 if($faded_image  = ImagesHelper::fade($image_source,$alpha_start,$alpha_end,$bg_color)) {
    # save resulted image in to file in png format
-   ImagesHelper::saveimage($faded_image, $image_source,'fad_','png');  
+   ImagesHelper::saveimage($faded_image, $image_source,'fad2_','png');  
 }
 
 # rotate image example 1 with transparent background
@@ -143,9 +173,57 @@ if($watermarked_image  = ImagesHelper::buildtextwatermark( $image_source, $text,
 
 # build image watemark
 $watermark_img = './images/wtm.png';
-if($watermarked_image  = ImagesHelper::buildimagewatermark($image_source, $watermark_img)) {
+if($watermarked_image = ImagesHelper::buildimagewatermark($image_source, $watermark_img)) {
    # save watermarked image in to file in gif format
    ImagesHelper::saveimage($watermarked_image, $image_source,'wtmi_','gif');    
 }
 
+/*
+'horizontal'
+'vertical'
+'ellipse'
+'ellipse2'
+'circle'
+'circle2'
+'square'
+'rectangle'
+'diamond'
+*/   
+$width = 200;
+$height = 100;
+$direction = 'ellipse';
+$start_color = '#5E0B5F';
+$end_color = '#fff';
+$step = 0;
+if($gradient_image = ImagesHelper::gradientfill($width,$height,$direction,$start_color,$end_color,$step)) {
+   # save watermarked image in to file in gif format
+   ImagesHelper::saveimage($gradient_image, $image_source,'gr1_','png');   
+}
+
+$width = 100;
+$direction = 'circle';
+if($gradient_image = ImagesHelper::gradientfill($width,$height,$direction,$start_color,$end_color,$step)) {
+   # save watermarked image in to file in gif format
+   ImagesHelper::saveimage($gradient_image, $image_source,'gr2_','png');   
+}
+
+$direction = 'horizontal';
+if($gradient_image = ImagesHelper::gradientfill($width,$height,$direction,$start_color,$end_color,$step)) {
+   # save watermarked image in to file in gif format
+   ImagesHelper::saveimage($gradient_image, $image_source,'gr3_','png');   
+}
+
+$direction = 'rectangle';
+if($gradient_image = ImagesHelper::gradientfill($width,$height,$direction,$start_color,$end_color,$step)) {
+   # save watermarked image in to file in gif format
+   ImagesHelper::saveimage($gradient_image, $image_source,'gr4_','png');   
+}
+
+$direction = 'square';
+if($gradient_image = ImagesHelper::gradientfill($width,$height,$direction,$start_color,$end_color,$step)) {
+   # build transparent
+   $gradient_image = ImagesHelper::tranparent($gradient_image,$start_color);
+   # save watermarked image in to file in gif format
+   ImagesHelper::saveimage($gradient_image, $image_source,'gr5_','png');   
+}
 ?>
